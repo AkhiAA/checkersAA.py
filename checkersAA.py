5c140e741ae6f088f6f7bbd6ab71ba9bc5a8b022
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-01 09:29:36 -0500

    made square red

--- checkersAA.py
+++ checkersAA.py
@@ -2,17 +2,20 @@
 
 from graphics import *
 
+def draw_sq(sX, sY, size, color, win):
+    square = Rectangle(Point(sX, sY), Point(sX * 2, sY * 2))
+    square.setFill(color)
+    square.draw(win)
+   
+
 sqSz = 50
 
 
 chWin =GraphWin("Checkers", sqSz*10, sqSz*10)
 chWin.setCoords(0, 0, sqSz*10, sqSz*10)
 
-sQ = Rectangle(Point(sqSz ,sqSz ),Point(sqSz * 2,sqSz * 2))
-    
-sQ.draw(chWin)
 
-
+draw_sq(sqSz, sqSz, sqSz, "red", chWin)
 
 
 chWin.getMouse()
