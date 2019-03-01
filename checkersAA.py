04130468901770846c0d64a4cc6cf0d7995187b0
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-01 09:45:57 -0500

    made row of squares and corrected function for sX

--- checkersAA.py
+++ checkersAA.py
@@ -3,7 +3,7 @@
 from graphics import *
 
 def draw_sq(sX, sY, size, color, win):
-    square = Rectangle(Point(sX, sY), Point(sX * 2, sY * 2))
+    square = Rectangle(Point(sX, sY), Point(sX + size, sY * 2))
     square.setFill(color)
     square.draw(win)
    
@@ -14,8 +14,8 @@ sqSz = 50
 chWin =GraphWin("Checkers", sqSz*10, sqSz*10)
 chWin.setCoords(0, 0, sqSz*10, sqSz*10)
 
-
-draw_sq(sqSz, sqSz, sqSz, "red", chWin)
+for i in range(8):
+    draw_sq(sqSz * (i + 1), sqSz, sqSz, "red", chWin)
 
 
 chWin.getMouse()
