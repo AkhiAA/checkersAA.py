a489f138d2a14323c04195334396aa8b6e0238b4
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-06 09:22:00 -0500

    added a variable for square color

--- checkersAA.py
+++ checkersAA.py
@@ -7,16 +7,15 @@ def draw_sq(sX, sY, size, color, win):
     square.setFill(color)
     square.draw(win)
    
-
 sqSz = 50
-
+sqCol = "red"
 
 chWin =GraphWin("Checkers", sqSz*10, sqSz*10)
 chWin.setCoords(0, 0, sqSz*10, sqSz*10)
 
 for j in range (8):
     for i in range(8):
-        draw_sq(sqSz * (i + 1), sqSz * (j+1), sqSz, "red", chWin)
+        draw_sq(sqSz * (i + 1), sqSz * (j+1), sqSz, sqCol, chWin)
      
 
 
